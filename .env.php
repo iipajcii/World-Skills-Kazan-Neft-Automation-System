47e0ea968dd473c5a8a30909f175baf3bc03e09e
Patrick <iipajcii@gmail.com>
2021-08-22 09:46:57 -0500

    chore(.env): Update env file with api routes

--- .env.php
+++ .env.php
@@ -10,6 +10,13 @@
 		'login' => $env_base_url.'/templates/pages/login.php',
 		'dashboard' => $env_base_url.'/templates/pages/dashboard.php'
 	];
+	$env_api = [
+		'assets' => $env_base_url.'/api/asset_list.php',
+		'create_emergency_maintenance' => $env_base_url.'/api/create_emergency_maintenance.php',
+		'asset_maintenance' => $env_base_url.'/api/asset_maintenance.php',
+		'parts' => $env_base_url.'/api/parts.php',
+		'asset_history' => $env_base_url.'/api/asset_history.php',
+	];
 	$env_functions = [
 		'login' => $env_base_url.'/functions/login.php'
 	];
