8597eb688f70a8f9797775d2b175d7ccfa59cad9
Patrick <iipajcii@gmail.com>
2021-08-22 09:44:37 -0500

    func(api): Create function to return all the parts stored in the system

---
+++ api/parts.php
@@ -0,0 +1,20 @@
+<?php
+	require "../.env.php";
+	$mysql = new mysqli($env_database['host'], $env_database['user'], $env_database['password'], $env_database['database']);
+	if ($mysql->connect_error) die($mysql->connect_error);
+
+	$query = "SELECT * FROM (parts)";
+	$result = $mysql->query($query);
+	if (!$result) die($mysql->error);
+	
+	$parts = [];
+	for($i = 0; $row = $result->fetch_assoc(); $i++) {
+		$parts[$i] = new stdClass();
+    	$parts[$i]->id = $row['ID'];
+    	$parts[$i]->Name = preg_replace('/[^A-Za-z0-9\-]/', ' ', $row['Name']);
+    	$parts[$i]->EffectiveLife = $row['EffectiveLife'];
+  	}
+
+	header('Content-Type: application/json');
+	echo json_encode($parts);
+?>
