b3a1fd485dc281aac2c13b5b12f64e452c6b56e4
Patrick <iipajcii@gmail.com>
2021-08-22 09:39:14 -0500

    chore(.env): Add database credentials to environment

--- .env.php
+++ .env.php
@@ -1,5 +1,11 @@
 <?php
 	$env_base_url = "http://localhost/WorldSkills";
+	$env_database = [
+		"user" => "world_skills",
+		"password" => "password",
+		"database" => "Section1",
+		"host" => "localhost"
+	];
 	$env_pages = [
 		'login' => $env_base_url.'/templates/pages/login.php',
 		'dashboard' => $env_base_url.'/templates/pages/dashboard.php'
