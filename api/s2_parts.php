ca3971ac8dbe3e38f16eb6bb032385378288fde0
Patrick <iipajcii@gmail.com>
2021-08-23 02:11:25 -0500

    func(api): Create function to return parts as required in section 2

---
+++ api/s2_parts.php
@@ -0,0 +1,22 @@
+<?php
+	require "../.env.php";
+	$mysql = new mysqli($env_database['host'], $env_database['user'], $env_database['password'], $env_databases['Section2']);
+	if ($mysql->connect_error) die($mysql->connect_error);
+
+	$query = "SELECT * FROM (parts)";
+	$result = $mysql->query($query);
+	if (!$result) die($mysql->error);
+	
+	$parts = [];
+	for($i = 0; $row = $result->fetch_assoc(); $i++) {
+		$parts[$i] = new stdClass();
+    	$parts[$i]->id = $row['ID'];
+    	$parts[$i]->Name = preg_replace('/[^A-Za-z0-9\-]/', ' ', $row['Name']);
+    	$parts[$i]->EffectiveLife = $row['EffectiveLife'];
+    	$parts[$i]->BatchNumberHasRequired = (bool)$row['BatchNumberHasRequired'];
+    	$parts[$i]->MinimumAmount = $row['MinimumAmount'];
+  	}
+
+	header('Content-Type: application/json');
+	echo json_encode($parts);
+?>
