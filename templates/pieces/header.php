220c37081c6aeb90d50eb6dc71e7591ebb7fef24
Patrick <iipajcii@gmail.com>
2021-08-23 19:10:49 -0500

    style(header): Change header of application

--- templates/pieces/header.php
+++ templates/pieces/header.php
@@ -1,3 +1,7 @@
+<?php
+	session_start();
+	require_once "../../.env.php";
+?>
 <header style="width:100%;">
 	<nav class="navbar" role="navigation" aria-label="main navigation">
 		<div class="navbar-brand">
@@ -12,18 +16,7 @@
 		</div>
 		<div id="navbarBasicExample" class="navbar-menu">
 			<div class="navbar-start">
-				<a class="navbar-item">Home</a>
-				<a class="navbar-item">Documentation</a>
-				<div class="navbar-item has-dropdown is-hoverable">
-					<a class="navbar-link">More</a>
-					<div class="navbar-dropdown">
-						<a class="navbar-item">About</a>
-						<a class="navbar-item">Jobs</a>
-						<a class="navbar-item">Contact</a>
-						<hr class="navbar-divider">
-						<a class="navbar-item">Report an issue</a>
-					</div>
-				</div>
+				<a href="<?php $env_pages['login']?>" class="navbar-item"></a>
 			</div>
 			<div class="navbar-end">
 				<div class="navbar-item">
