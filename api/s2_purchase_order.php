6f2e282b4fe69ed4461b7847003a444dddf822f6
Patrick <iipajcii@gmail.com>
2021-08-23 19:02:05 -0500

    func(api): Create function to add purchase order in orders table

---
+++ api/s2_purchase_order.php
@@ -0,0 +1,52 @@
+<?php
+	require "../.env.php";
+	header('Content-Type: application/json');
+
+	$supplier = $_POST['supplier']; 
+	$destination_warehouse = $_POST['destination_warehouse']; 
+	$date = $_POST['date']; 
+	$parts = $_POST['parts']; 
+
+	if(isset($_POST['supplier']) && isset($_POST['destination_warehouse'])){
+		$transaction_type = 1;
+	}
+
+	$mysql = new mysqli($env_database['host'], $env_database['user'], $env_database['password'], $env_databases['Section2']);
+	if ($mysql->connect_error) die($mysql->connect_error);
+	$query = "INSERT INTO orders (TransactionTypeID, SupplierID, SourceWarehouseID, DestinationWarehouseID, `Date`) VALUES ($transaction_type, $supplier, NULL, $destination_warehouse, '$date')";
+	$result = $mysql->query($query);
+	if (!$result) die($mysql->error);
+
+	$query = "SELECT * FROM orders";
+	$result = $mysql->query($query);
+	if (!$result) die($mysql->error);
+
+	//Get Highest id in orders
+	$order_id = 0;
+	for($i = 0; $row = $result->fetch_assoc(); $i++) {
+		if($row['ID'] > $order_id){$order_id = $row['ID'];}
+	}
+
+	$parts = json_decode($parts);
+
+	foreach($parts as $part) {
+		$query = "INSERT INTO orderitems (`OrderID`, `PartID`, `BatchNumber`, `Amount`) VALUES ($order_id, $part->id, '$part->batch', $part->amount)";
+		$result = $mysql->query($query);
+		if (!$result) die($mysql->error);		
+	}
+
+
+	$msg = new stdClass();
+
+	if($result){
+		$msg->message = "Order and Parts Created";
+		$msg->value = true;
+		echo json_encode($msg); exit;
+	}
+	else {
+		$msg->message = "Order and Parts could not be created";
+		$msg->error = $mysql->error;
+		$msg->value = false;
+		echo json_encode($msg); exit;
+	}
+?>
